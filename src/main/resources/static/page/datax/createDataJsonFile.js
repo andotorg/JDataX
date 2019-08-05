17617512d934cd5bb9986128b91a3fdf1c5bea8e
andot <yanfront@qq.com>
2019-08-05 20:19:01 +0800

    增加websocket实时回传

--- src/main/resources/static/page/datax/createDataJsonFile.js
+++ src/main/resources/static/page/datax/createDataJsonFile.js
@@ -104,7 +104,56 @@ layui.use(['form','layer','laydate','table','laytpl'],function(){
 	      }
 		});
     })
-    
+
+	/***
+	 * Websocket 实现实时回传控制台日志
+	 *
+	 */
+	let websocket = null;
+	// 判断当前浏览器是否支持WebSocket
+	if ('WebSocket' in window) {
+		websocket = new WebSocket("ws://127.0.0.1:8080/webSocket");
+	} else {
+		alert('当前浏览器不支持WebSocket')
+	}
+
+	// 连接发生错误的回调方法
+	websocket.onerror = function () {
+		setMessageInnerHTML("WebSocket连接发生错误");
+	};
+
+	// 连接成功建立的回调方法
+	websocket.onopen = function () {
+		//setMessageInnerHTML("WebSocket连接成功");
+	}
+
+	// 接收到消息的回调方法
+	websocket.onmessage = function (event) {
+		setMessageInnerHTML(event.data);
+	}
+
+	// 连接关闭的回调方法
+	websocket.onclose = function () {
+		setMessageInnerHTML("WebSocket连接关闭");
+	}
+
+	// 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+	window.onbeforeunload = function () {
+		closeWebSocket();
+	}
+
+	// 将消息显示在网页上
+	function setMessageInnerHTML(text) {
+		let obj = document.getElementById("message");
+		obj.value += text + "\n";
+		obj.scrollTop = obj.scrollHeight;
+	}
+
+	// 关闭WebSocket连接
+	function closeWebSocket() {
+		websocket.close();
+	}
+
     $(".datax_Btn").click(function(){
     	layui.layer.open({
   		  type: 1,
@@ -157,7 +206,10 @@ layui.use(['form','layer','laydate','table','laytpl'],function(){
 	  		    	layui.layer.msg("异常发生，参数必填，请检查在执行。");
 	  		      }
 	  	  	  });
-  		  }
+  		  },
+		  btn2: function () {
+			  closeWebSocket();
+		  },
     	});
     	
     });
